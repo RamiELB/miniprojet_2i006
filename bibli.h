e2d806487a190f3b4475d7ca3259a6188a278539
= <rami.elbeblawy@gmail.com>
2019-02-14 21:28:56 +0100

    ajouts commentaires

--- bibli.h
+++ bibli.h
@@ -17,17 +17,27 @@ typedef struct {
     int nbliv ; /* Nombre de livres dans la liste */
 } Biblio ;
 
-void initialise_biblio(Biblio *b);
-void ajouter_livre(Biblio *b, s_livre *l);
-s_livre *creer_livre(int num, char *titre, char *auteur);
-void lecture_n_entree(char *nom_fichier, int nb_lignes, Biblio *b);
 
-void afficher_livre(s_livre *l);
+void initialise_biblio(Biblio *b); //initialise correctement les champs de la structure à des valeures nulles ou équivalentes
 
-s_livre *recherche_par_numero(int n, Biblio *b, int appel);
-s_livre *recherche_par_titre(char *titre, Biblio *b);
-void recherche_auteur(char *auteur, Biblio *b);
+void ajouter_livre(Biblio *b, s_livre *l);  //ajouter d'un livre dans une bibliothèque
 
-void supprimer_livre(int num, Biblio *b);
-s_livre *recherche_doublons(Biblio *b);
+s_livre *creer_livre(int num, char *titre, char *auteur); //créée et renvoie un livre
+
+void lecture_n_entree(char *nom_fichier, int nb_lignes, Biblio *b); //lis n lignes dans un fichier, créée les livres et les ajoutes dans la bibliothèque
+
+void afficher_livre(s_livre *l); //affiche un livre
+
+s_livre *recherche_par_numero(int n, Biblio *b, int appel); //recherche d'un livre par son numéro
+/*l'argument appel sert à n'afficher que le livre n'est pas disponible uniquement si la fonction
+      est appelée explicitement depuis le menu */
+
+s_livre *recherche_par_titre(char *titre, Biblio *b); //recherche d'un livre par son titre
+
+void recherche_auteur(char *auteur, Biblio *b); //recherche tous les livres d'un auteur et les affiches
+
+void supprimer_livre(int num, Biblio *b); //supression d'un livre dans une bibliothèque par son numéro
+
+s_livre *recherche_doublons(Biblio *b); // renvoie une lise chainee de tous les livres présents au moins en double
+
 #endif
--- menu.h
+++ menu.h
@@ -4,10 +4,14 @@
 #include <stdlib.h>
 #include "bibli.h"
 
-void affichage_menu();
-void choix_menu(Biblio *b);
-void afficher_liste_livres(s_livre *liste);
-void clear_buffer();
-void ajout_livre_utilisateur(Biblio *b);
+void affichage_menu(); //afficher le menu
+
+void choix_menu(Biblio *b); //lis le choix du menu et fait appel aux fonctions correspondantes
+
+void afficher_liste_livres(s_livre *liste); //affiche tous les livres d'une liste chainee
+
+void clear_buffer(); //permet de ne pas avoir de bug, à utiliser apres un scanf
+
+void ajout_livre_utilisateur(Biblio *b); //fonction permettant à l'utilisateur d'ajouter un livre
 
 #endif
